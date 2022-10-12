1eaa2dd1f332cbe519509ddfa7013b36ba99fb90
oriolraventosc <oriol.raventos.c@gmail.com>
2022-10-12 18:37:04 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["xo", "prettier"],
+  overrides: [
+    {
+      extends: ["xo-typescript", "prettier"],
+      files: ["*.ts", "*.tsx"],
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "no-unused-vars": [
+      "error",
+      {
+        vars: "all",
+        args: "after-used",
+        ignoreRestSiblings: true,
+        argsIgnorePattern: /^_/.source,
+        caughtErrors: "all",
+        caughtErrorsIgnorePattern: /^_$/.source,
+      },
+    ],
+  },
+};
